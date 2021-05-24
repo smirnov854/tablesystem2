96c4593e109a2ffbe4b8b7478c221028279a4180
Smirnov <smirnov_e87@mail.ru>
2021-05-24 20:07:24 +0300

    create_project

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,7 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
- 
+    /****
+     * tesst
+     */
     private $error = "";
 
     public function __construct()
