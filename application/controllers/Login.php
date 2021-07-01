ee561b5282a25d3db0cd036b958fb0273d969fe1
Smirnov <smirnov_e87@mail.ru>
2021-07-01 22:06:12 +0300

    work_list pagination

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,7 +8,7 @@ class Login extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model("user_model");
+        $this->load->model("user_model"); 
     }
 
     public function index()
