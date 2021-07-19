a484bf9d5308a83926cb91f37c4df3e85ff5fea4
Smirnov <smirnov_e87@mail.ru>
2021-07-19 21:29:01 +0300

    Fix several bugs

--- application/views/work/work_list.php
+++ application/views/work/work_list.php
@@ -93,7 +93,7 @@
                     <textarea v-if="user_role_id==4 && request.done_work==''" class="form-control" v-model="request.cur_comment">{{request.done_work}}</textarea>
                     <button class="btn btn-success btn-sm" v-if="user_role_id==4 && request.done_work==''" v-on:click="save_cur_comment(request.id,index)"><i class="fa fa-check"></i></button>
                     <button class="btn btn-danger btn-sm" v-if="user_role_id==4 && request.done_work==''" v-on:click="request.cur_comment=''"><i class="fa fa-times"></i></button>
-                    {{request.done_work}}
+                    <span v-if="user_role_id!=4 && request.done_work!=''">{{request.done_work}}</span>
                 </div>
             </div>
             <div class="block  col-lg-4 col-md-4 col-sm-6 float-left img_container">
