62ead7ff7a37043bd69f8293db424f99532e30de
Smirnov <smirnov_e87@mail.ru>
2021-07-26 00:26:30 +0300

    Fix several bugs

--- application/views/work/gallery_modal.php
+++ application/views/work/gallery_modal.php
@@ -1,13 +1,12 @@
 <div id="cur_photo_dialog" class="modal  fade" role="dialog" tabindex="-1" aria-hidden="true" >
     <div class="modal-dialog modal-lg" role="document" >
-        <div class="modal-content" style="max-height: 80%">
+        <div class="modal-content" style="max-height: 100%">
             <div class="modal-header">
                 <div class="modal-title">
                 </div>
             </div>
             <div class="modal-body">
-                <img style='max-height: 700px;max-width: 700px;object-fit: cover' v-bind:src="cur_photo">
-                <img style='max-height: 100%;max-width: 100%;object-fit: cover' v-bind:src="cur_photo">
+                <img style='max-height: 100%;max-width: 100%;object-fit: cover' v-bind:src="cur_photo">                
             </div> 
             <div class="modal-footer">
                 <button class="btn btn-danger close_dialog float-left" data-dismiss="modal">Закрыть</button>
